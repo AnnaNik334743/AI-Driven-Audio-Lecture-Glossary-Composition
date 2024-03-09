4c190eab7feb40a8c8940244959683e50e9ff352
AnnaNik334743 <annanikiforova564@gmail.com>
2024-03-09 09:28:53 +0300

    Fixed list conversion

--- llm_service/app/api/utils.py
+++ llm_service/app/api/utils.py
@@ -28,7 +28,7 @@ def split_list_into_sublists(lst: list, n: int, overlap_length: int = 0) -> list
 
 async def split_text_if_it_is_too_long(text: str, chunk_size_in_words: int = 512, overlap: int = 0) -> list[str]:
     words = text.split()  # you can change it to a better tokenizer (or segmenter) if needed
-    return split_list_into_sublists(words, chunk_size_in_words, overlap)
+    return [' '.join(subwords) for subwords in split_list_into_sublists(words, chunk_size_in_words, overlap)]
 
 
 async def turn_str_to_glossary_parts(glossary_parts: str,
@@ -59,7 +59,7 @@ async def turn_str_to_glossary_parts(glossary_parts: str,
         return []
 
 
-async def turn_glossary_parts_to_glossary(glossary_parts: list[GlossaryItem] | str) -> Glossary:
+async def turn_glossary_parts_to_glossary(glossary_parts: list[GlossaryItem]) -> Glossary:
     try:
         return Glossary(glossary=glossary_parts)
     except Exception:
