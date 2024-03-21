eab03d029a5e5af98d06e3e552d06fbbb80119d2
elderberry17 <bezgin14a@mail.ru>
2024-03-21 23:14:36 +0300

    add chunks transcription

--- asr_service/app/api/asr.py
+++ asr_service/app/api/asr.py
@@ -1,11 +1,36 @@
-import whisper
-from api.asr_utils import download_and_convert_audio, whisper_transcribe_file
+import torch
+from api.asr_utils import download_and_convert_audio, transcribe_audio_by_chunks, whisper_transcribe_file
 from api.schema import Transcript
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 asr = APIRouter()
-# should load it from config
-whisper_model = whisper.load_model("small")
+
+# SHOULD LOAD IT FROM CONFIG
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+model_id = "openai/whisper-small"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+whisper_pipeline = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
 
 
 @asr.get("/", response_model=str)
@@ -15,14 +40,30 @@ async def index():
 
 @asr.post("/transcribe_file", response_model=Transcript)
 async def transcribe_file(youtube_link) -> Transcript:
-    # Custom validation logic for YouTube link
+    # youtube link validation
     if not youtube_link.startswith("https://www.youtube.com/"):
         raise HTTPException(status_code=400, detail="Некорректная ссылка")
 
     try:
         download_and_convert_audio(youtube_link, output_name="lecture.mp3")
-        transcript = whisper_transcribe_file(whisper_model, path_to_mp3_file="lecture.mp3", delete=True)
+        transcript = whisper_transcribe_file(whisper_pipeline, path_to_mp3_file="lecture.mp3", delete=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+    if not transcript:
+        return HTTPException(status_code=400, detail="Некорректная ссылка")
+
     return Transcript(transcript=transcript)
+
+
+@asr.post("/transcribe_file_chunks")
+async def transcribe_file_chunks(youtube_link):
+    if not youtube_link.startswith("https://www.youtube.com/"):
+        raise HTTPException(status_code=400, detail="Некорректная ссылка")
+
+    try:
+        download_and_convert_audio(youtube_link, output_name="lecture.mp3")
+        return StreamingResponse(
+            transcribe_audio_by_chunks(whisper_pipeline, input_file="lecture.mp3", chunk_duration=30))
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
--- asr_service/app/api/asr_utils.py
+++ asr_service/app/api/asr_utils.py
@@ -6,6 +6,9 @@ from pytube import YouTube
 
 
 def download_and_convert_audio(video_link, output_name="lecture.mp3") -> None:
+    if os.path.exists(output_name):
+        shutil.rmtree(output_name, ignore_errors=True)
+
     yt = YouTube(video_link)
 
     video = yt.streams.filter(only_audio=True).first()
@@ -20,20 +23,24 @@ def download_and_convert_audio(video_link, output_name="lecture.mp3") -> None:
     os.remove(downloaded_file)
 
 
-def whisper_transcribe_file(model, path_to_mp3_file="lecture.mp3", delete=True) -> str:
-    transcription = model.transcribe(path_to_mp3_file, language="ru", verbose=False)
-
-    print("DONE:", transcription)
+def whisper_transcribe_file(whisper_pipeline, path_to_mp3_file="lecture.mp3", delete=True) -> str:
+    result = whisper_pipeline(path_to_mp3_file)
 
     if delete:
         shutil.rmtree(path_to_mp3_file, ignore_errors=True)
 
-    return transcription["text"]
+    if len(result) > 0:
+        return result['text']
+
+    return ""
 
 
-def split_mp3_into_chunks(input_file, chunk_duration=300, chunks_dir="output_chunks"):
+def split_mp3_into_chunks(input_file, chunk_duration=120, chunks_dir="output_chunks"):
+    if os.path.exists(chunks_dir):
+        shutil.rmtree(chunks_dir, ignore_errors=True)
+
     os.makedirs(chunks_dir)
-    audio = AudioSegment.from_file(input_file, format="mp4")
+    audio = AudioSegment.from_file(input_file, format="mp3")
 
     chunk_size = chunk_duration * 1000
 
@@ -43,13 +50,19 @@ def split_mp3_into_chunks(input_file, chunk_duration=300, chunks_dir="output_chu
         chunk.export(output_file, format="mp3")
 
 
-def transcribe_audio_by_chunks(whisper_model, input_file, chunk_duration=300, input_dir="output_chunks",
+def transcribe_audio_by_chunks(whisper_pipeline, input_file, chunk_duration=120, input_dir="output_chunks",
                                delete_dir=True):
     split_mp3_into_chunks(input_file, chunk_duration)
+    chunk_files = os.listdir(input_dir)
 
-    for chunk_file in os.listdir(input_dir):
-        segment = whisper_transcribe_file(whisper_model, path_to_mp3_file=os.path.join(input_dir, chunk_file))
+    for ind, chunk_file in enumerate(chunk_files):
+        # динамически удаляем предыдущие чанки
+        if ind > 0 and delete_dir:
+            shutil.rmtree(os.path.join(input_dir, chunk_files[ind - 1]), ignore_errors=True)
+
+        segment = whisper_transcribe_file(whisper_pipeline, path_to_mp3_file=os.path.join(input_dir, chunk_file))
+        # в конце убираем всю директорию
+        if ind == len(chunk_files) - 1 and delete_dir:
+            shutil.rmtree(input_dir, ignore_errors=True)
+
         yield segment
-
-    if delete_dir:
-        shutil.rmtree(input_dir, ignore_errors=True)
